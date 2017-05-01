1ff8cf206dfc200870e7dcd25061516a08594c2b
郑俊鑫 <18810544291@163.com>
2017-05-01 17:15:09 +0800

    成功删除返回ret 0

--- controllers/user.js
+++ controllers/user.js
@@ -5,12 +5,10 @@ exports.deleteParty = function(req, res) {
     const partyId = req.params.id
     const userId = getUserId(req)
 
-    console.log('-', partyId, userId)
     User.removePartyId(userId, partyId)
         .then(() => {
-            console.log('---')
             res.send({
-                ret: 1,
+                ret: 0,
             })
         })
 }
